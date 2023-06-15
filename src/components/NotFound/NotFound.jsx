c92c867a78927b502ef2076a7b538f05f48203d6
Alber <albertorg0087@gmail.com>
2023-06-15 12:57:47 +0100

    Primary and Back buttons created and styled

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,8 +1,10 @@
+import BackBtn from '../BackBtn/BackBtn';
 import './NotFound.css';
 
 function NotFound() {
   return (
     <div className="not-found">
+      <BackBtn />
       <div>Oops! Page not found!</div>
     </div>
   )
