6573b5728bac764c262189ad08458197cb3b8152
Adrián <adrianglezm97@gmail.com>
2023-06-20 16:37:48 +0100

    Merge branch 'adri' into development

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://roomiesapi-production.up.railway.app/api',
+  baseURL: 'https://roomiesapi-production.up.railway.app/api',
   timeout: 3000,
 })
