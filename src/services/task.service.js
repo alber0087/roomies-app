9422368f71a594197dbbd77adb01427ba604e7c7
ayo <ayopro89@gmail.com>
2023-06-20 17:05:14 +0100

    added service getTasks

--- src/services/task.service.js
+++ src/services/task.service.js
@@ -1,6 +1,9 @@
 import { api } from './api'
 
 export const getTasks = async () => {
-  const { data } = await api.get('/tasks')
-  return data
+  const { data } = await api.get('/tasks/profile/tasks', {
+      headers: {
+        token: localStorage.getItem('token'),
+      }})
+  return data.tasks
 }
